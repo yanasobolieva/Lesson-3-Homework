f09a190587dafcc6876b0d57c79775bb7b1f8c26
yanasobolieva <soboleva.yana.sys@gmail.com>
2019-10-27 15:53:09 +0200

    Homework after lesson 3

---
+++ homework.py
@@ -0,0 +1,16 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int * 3.5
+var_float -= 1
+
+print(var_int / var_float)
+print(big_int / var_float)
+
+var_str = var_str*2 + "Yes"*3
+
+print(var_int)
+print(var_float)
+print(var_str)
+print(big_int)
